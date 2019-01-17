2f05cf5bd8c263e4aca91eaec96e5f72c7919332
chrisisler <christopherisler1@gmail.com>
2019-01-17 09:02:02 -0500

    working on Header component and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react'
 import './App.css'
 
-class App extends Component {
+function Header() {
+  return (
+    <nav class="header-component">
+      <ol class="header-links-list">
+        <li>
+          <a href="foo1">bar1</a>
+        </li>
+        <li>
+          <a href="foo2">bar2</a>
+        </li>
+      </ol>
+    </nav>
+  )
+}
+
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        {/* <Rest /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </div>
     )
   }
 }
-
-export default App
